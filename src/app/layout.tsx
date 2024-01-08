8106e7b08fb7683f85aa2dace138e7c193967d96
euvitordev <dev.vitorlucas@gmail.com>
2024-01-08 00:57:42 -0300

    Correction: Error converting to String and adjusting the site's metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Inicio | Vitor Lucas",
+  title: "Vitor Lucas | Desenvolvedor Front End",
   description: "Em breve",
 };
 
